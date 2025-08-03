c1c16cb95dd28277c2fa82842837c5565683c838
ashutosh119 <ashutoshmca11@gmail.com>
2025-08-04 01:56:13 +0530

    new file changes

---
+++ src/main/java/com/spring/ElectronicsProductApplication.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicsProductApplication.class, args);
+		System.out.println("Electronics product");
+	}
+
+}
